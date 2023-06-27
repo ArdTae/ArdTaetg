cfaf5f9ac6f75657d59cc0424aa303e5e8ccfdcf
ArdTae <137835319+ArdTae@users.noreply.github.com>
2023-06-27 22:36:51 +0500

    Installer.sh
    This file launches

---
+++ installer.sh
@@ -0,0 +1,15 @@
+echo -e "Install python..."
+pkg install python
+echo -e "Install python 2..."
+pkg install python2
+echo -e "Install git..."
+apt install git
+echo -e "Install pip"
+pkg install python-pip
+echo -e "Install telebot"
+pip install pyTelegramBotAPI
+echo -e "Install requests"
+pip install requests
+echo -e "Install loguru"
+pip install loguru
+echo -e "Everything is installed successfully [7/7]"
